48d91cbc0a89ec3a53d5b859d9513ff531cb182f
bas1c <68533770+bas1c1@users.noreply.github.com>
2023-10-02 16:02:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 from flask import Flask
+from flask import send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
-
-import os
-from flask import send_from_directory
-
-static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static')
-
-@app.route('/<path:path>', methods=['GET'])
-def serve_file_in_dir(path):
-
-    if not os.path.isfile(os.path.join(static_file_dir, path)):
-        path = os.path.join(path, 'GTA_SA.zip')
-
-    return send_from_directory(static_file_dir, path)
-
-app.run(host='0.0.0.0',port=8080)
-
+    return app.send_static_file('GTA_SA.zip')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=True, port=8080)
